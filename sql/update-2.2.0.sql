63875b7d7321e69db159aefdaa3dcea389eea3e7
tsmr <thetsmr@gmail.com>
2022-04-07 17:12:24 +0200

    update sql

--- sql/update-2.2.0.sql
+++ sql/update-2.2.0.sql
@@ -1,12 +1,12 @@
-ALTER TABLE `glpi_plugin_eventsmanager_events` CHANGE `date_mod` `date_mod` TIMESTAMP NULL default '0000-00-00 00:00:00';
-ALTER TABLE `glpi_plugin_eventsmanager_events` CHANGE `date_creation` `date_creation` TIMESTAMP NULL default '0000-00-00 00:00:00';
-ALTER TABLE `glpi_plugin_eventsmanager_events` CHANGE `date_assign` `date_assign` TIMESTAMP NULL default '0000-00-00 00:00:00';
-ALTER TABLE `glpi_plugin_eventsmanager_events` CHANGE `date_close` `date_close` TIMESTAMP NULL default '0000-00-00 00:00:00';
-ALTER TABLE `glpi_plugin_eventsmanager_events` CHANGE `date_ticket` `date_ticket` TIMESTAMP NULL default '0000-00-00 00:00:00';
-ALTER TABLE `glpi_plugin_eventsmanager_events` CHANGE `time_to_resolve` `time_to_resolve` TIMESTAMP NULL default '0000-00-00 00:00:00';
+ALTER TABLE `glpi_plugin_eventsmanager_events` CHANGE `date_mod` `date_mod` TIMESTAMP NULL default NULL;
+ALTER TABLE `glpi_plugin_eventsmanager_events` CHANGE `date_creation` `date_creation` TIMESTAMP NULL default NULL;
+ALTER TABLE `glpi_plugin_eventsmanager_events` CHANGE `date_assign` `date_assign` TIMESTAMP NULL default NULL;
+ALTER TABLE `glpi_plugin_eventsmanager_events` CHANGE `date_close` `date_close` TIMESTAMP NULL default NULL;
+ALTER TABLE `glpi_plugin_eventsmanager_events` CHANGE `date_ticket` `date_ticket` TIMESTAMP NULL default NULL;
+ALTER TABLE `glpi_plugin_eventsmanager_events` CHANGE `time_to_resolve` `time_to_resolve` TIMESTAMP NULL default NULL;
 
-ALTER TABLE `glpi_plugin_eventsmanager_origins` CHANGE `date_mod` `date_mod` TIMESTAMP NULL default '0000-00-00 00:00:00';
-ALTER TABLE `glpi_plugin_eventsmanager_origins` CHANGE `date_creation` `date_creation` TIMESTAMP NULL default '0000-00-00 00:00:00';
+ALTER TABLE `glpi_plugin_eventsmanager_origins` CHANGE `date_mod` `date_mod` TIMESTAMP NULL default NULL;
+ALTER TABLE `glpi_plugin_eventsmanager_origins` CHANGE `date_creation` `date_creation` TIMESTAMP NULL default NULL;
 
-ALTER TABLE `glpi_plugin_eventsmanager_events_comments` CHANGE `date_creation` `date_creation` TIMESTAMP NULL default '0000-00-00 00:00:00';
-ALTER TABLE `glpi_plugin_eventsmanager_events_comments` CHANGE `date_mod` `date_mod` TIMESTAMP NULL default '0000-00-00 00:00:00';
+ALTER TABLE `glpi_plugin_eventsmanager_events_comments` CHANGE `date_creation` `date_creation` TIMESTAMP NULL default NULL;
+ALTER TABLE `glpi_plugin_eventsmanager_events_comments` CHANGE `date_mod` `date_mod` TIMESTAMP NULL default NULL;
