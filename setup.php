1c2546e51094da2de44a98fa8e8959eaa70d1a03
Thetsmr <thetsmr@gmail.com>
2026-08-23 11:04:49 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -39,7 +39,7 @@ use GlpiPlugin\Mydashboard\Alert;
 
 global $CFG_GLPI;
 
-define('PLUGIN_EVENTMANAGER_VERSION', '4.0.1');
+define('PLUGIN_EVENTMANAGER_VERSION', '4.0.2');
 
 if (!defined("PLUGIN_EVENTMANAGER_DIR")) {
     define("PLUGIN_EVENTMANAGER_DIR", Plugin::getPhpDir("eventsmanager"));
